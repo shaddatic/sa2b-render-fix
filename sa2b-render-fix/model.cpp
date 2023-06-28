a54f8a8ebbf92e7a63bdb039cd9f5a9f28f7b484
Shaddatic <Shaddatic@gmail.com>
2023-06-29 00:55:41 +0100

    Renamed `models.cpp` and `models` path to just `model` to match upcomming `texture` path

---
+++ sa2b-render-fix/model.cpp
@@ -0,0 +1,36 @@
+#include <sa2b/core.h>
+
+#include <sa2b/ninja/ninja.h>
+#include <sa2b/ninja/njdef.h>
+
+#pragma warning( push )
+#pragma warning( disable : 4838 )	/* conversion from '$type' to '$type' requires a narrowing conversion */
+#pragma warning( disable : 4309 )	/* 'initializing': truncation of constant value */
+
+/*
+*	Models
+*/
+#include <model/e_kumi_emblem.nja>
+#include <model/e_g_kumi_emblem.nja>
+#include <model/e_b_kumi_emblem.nja>
+#include <model/e_gold_emblem.nja>
+#include <model/e_kyoko_emblem.nja>
+
+#include <model/e_shouko_wing_port.nja>
+#include <model/e_shouko_wing_starboard.nja>
+#include <model/e_shouko_aft.nja>
+
+#include <model/e_g_pathkumi_emblem.nja>
+#include <model/e_b_pathkumi_emblem.nja>
+
+#include <model/e_t_emi_emblem.nja>
+#include <model/e_a_emi_emblem.nja>
+
+#include <model/basic_shadow.nja>
+
+/*
+*	Ginja
+*/
+
+
+#pragma warning( pop )
