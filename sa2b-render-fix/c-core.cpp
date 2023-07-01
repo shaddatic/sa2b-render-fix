b58ceaf7477646fb3716e8af22a439074499890b
Shaddatic <Shaddatic@gmail.com>
2023-07-01 11:32:50 +0100

    Add Cannons Core source file, add Rouge pitfall restore

---
+++ sa2b-render-fix/c-core.cpp
@@ -0,0 +1,28 @@
+#include <sa2b/core.h>
+#include <sa2b/config.h>
+#include <sa2b/model.h>
+
+#include <sa2b/src/landtable.h>
+#include <sa2b/src/misc.h>
+
+#define SECT "c_core"
+
+#define ReplaceLandObject(pland, idx, pobj) ((OBJ_LANDTABLE*)pland)->pLandEntry[idx].pObj.gjs = pobj
+
+static void
+PitFallRougeRestore()
+{
+	OBJ_LANDTABLE* land = GetDllLandTable("objLandTable0037");
+	GJS_OBJECT* object = LoadGinjaObject2(GetModPath(), "model/stg37_land0032.sa2bmdl");
+
+	ReplaceLandObject(land, 32, object);
+}
+
+void
+CannonsCoreSettings(const config* conf)
+{
+	if (ConfigGetInt(conf, SECT, "pitfall", 1))
+	{
+		PitFallRougeRestore();
+	}
+}
