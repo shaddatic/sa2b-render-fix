9e1af5956d98c96650f05f7e99c86e8992676731
Shaddatic <Shaddatic@gmail.com>
2023-07-01 11:30:18 +0100

    Seperate out into multiple headers, add new 'Settings' functions

--- sa2b-render-fix/render-fix.h
+++ sa2b-render-fix/render-fix.h
@@ -1,74 +1,25 @@
 #pragma once
 
-#include <sa2b/core.h>
-
-#include <sa2b/ninja/ninja.h>
-
 #define arylen(arry)	(sizeof(arry) / sizeof(*arry))
 
-/*
-*	Enemy Jet
-*/
-
-void	EnemyJetDisplayerFix();
+void	ExtraShadowEnable();
 
 /*
-*	Tint Disable
+*	Sections
 */
 
-void	DefaultGinjaDiffuseEnable();
+void	RestorationSettings(const config* conf);
 
-void	DisableTintGinjaModel(GJS_MODEL* pModel);
-void	DisableTintGinjaObject(GJS_OBJECT* pObject);
+void	EnvMapSettings(const config* conf);
 
-void	DisableTintChunkModel(NJS_CNK_MODEL* pModel);
-void	DisableTintChunkObject(NJS_CNK_OBJECT* pObject);
+void	GetEmblemSettings(const config* conf);
 
-void	DisableTintGinjaObjectList(int* pList, int nbList);
-void	DisableTintGinjaModelList(int* pList, int nbList);
+void	StageMapSettings(const config* conf);
 
-void	DisableTintChunkObjectList(int* pList, int nbList);
-void	DisableTintChunkModelList(int* pList, int nbList);
+void	CityEscapeSettings(const config* conf);
 
-#define DisableTintGinjaObjectListEx(list)	DisableTintGinjaObjectList(list, arylen(list))
-#define DisableTintGinjaModelListEx(list)	DisableTintGinjaModelList(list, arylen(list))
+void	AquaticMineSettings(const config* conf);
 
-#define DisableTintChunkObjectListEx(list)	DisableTintChunkObjectList(list, arylen(list))
-#define DisableTintChunkModelListEx(list)	DisableTintChunkModelList(list, arylen(list))
+void	CannonsCoreSettings(const config* conf);
 
-/*
-*	objpak
-*/
-
-enum
-{
-	TINTMD_KEEP,
-	TINTMD_DISABLE
-};
-
-int		EditObjPak(bool tintDisable, float32 shadowOpacity);
-
-/*
-*	Stage Map
-*/
-
-void	StageMapTextBackdropRestore();
-void	StageMapStretchFix();
-
-/*
-*	GUN Emblem
-*/
-
-void	GUNEmblemRestore();
-
-/*
-*	Shadows
-*/
-
-void	EnemyShadowEnable();
-
-/*
-*	Light Mod
-*/
-
-void	LightModifierFix();
+void	BossBogySettings(const config* conf);
