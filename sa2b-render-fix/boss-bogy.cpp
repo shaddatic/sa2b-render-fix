7f5a1c0cde8dcbc9ca9e7c672308a1bc857afe80
Shaddatic <Shaddatic@gmail.com>
2023-07-01 11:32:25 +0100

    Add Boom Boo source file, add Sun Doors restoration

---
+++ sa2b-render-fix/boss-bogy.cpp
@@ -0,0 +1,21 @@
+#include <sa2b/core.h>
+#include <sa2b/config.h>
+
+#include <tools.h>
+
+#define SECT "bossbogy"
+
+static void
+SunDoorsFix()
+{
+	SwapGVMTableIndex(0x0103622C, 2, 4);
+}
+
+void
+BossBogySettings(const config* conf)
+{
+	if (ConfigGetInt(conf, SECT, "sundoors", 1))
+	{
+		SunDoorsFix();
+	}
+}
