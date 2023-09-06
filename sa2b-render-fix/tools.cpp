f354caa84070a99aac1586887e2f3edc27fecc91
Shaddatic <Shaddatic@gmail.com>
2023-09-06 05:57:56 +0100

    Added a ton of new tool funcs, and function overloading to solve C++ caused formatting headaches

--- sa2b-render-fix/tools.cpp
+++ sa2b-render-fix/tools.cpp
@@ -4,14 +4,531 @@
 #include <sa2b/ninja/ninja.h>
 
 #include <sa2b/src/texture.h>
+#include <sa2b/src/landtable.h>
+
+#include <tools.h>
 
 void
-SwapGVMTableIndex(int pGvmTable, int idx1, int idx2)
+FlipCnkModelWinding(NJS_CNK_MODEL* pModel)
 {
-	TEX_GVMTABLE* gvmtable = (TEX_GVMTABLE*)pGvmTable;
-	TEX_GVMTABLE tmp = gvmtable[idx1];
-	gvmtable[idx1] = gvmtable[idx2];
-	gvmtable[idx2] = tmp;
+    sint16* plist = pModel->plist;
+    int type;
+
+    while (1)
+    {
+        while (1)
+        {
+            while (1)
+            {
+                while (1)
+                {
+                    while (1)
+                    {
+                        while (1)
+                        {
+                            type = ((uint8*)plist)[0];
+
+                            if (type >= NJD_BITSOFF)
+                                break;
+
+                            ++plist;
+                        }
+
+                        if (type >= NJD_TINYOFF)
+                            break;
+
+                        ++plist;
+                    }
+
+                    if (type >= NJD_MATOFF)
+                        break;
+
+                    plist += 2;
+                }
+
+                if (type >= NJD_VERTOFF)
+                    break;
+
+                plist += 2;
+
+                switch (type) {
+                case NJD_CM_D:
+                case NJD_CM_A:
+                case NJD_CM_S:
+                    plist += 2;
+                    break;
+                case NJD_CM_DA:
+                case NJD_CM_DS:
+                case NJD_CM_AS:
+                    plist += 4;
+                    break;
+                case NJD_CM_DAS:
+                    plist += 6;
+                    break;
+                }
+            }
+
+            if (type >= NJD_STRIPOFF)
+                break;
+        }
+
+        if (type == NJD_ENDOFF)
+            break;
+
+        int nbstrip = plist[2] & 0x3FFF;
+
+        plist += 3;
+
+        while (nbstrip--)
+        {
+            int len = *plist;
+
+            *plist = -len;
+
+            ++plist;
+
+            if (len < 0)
+                len = -len;
+
+            plist += len * 3;
+        }
+    }
+}
+
+void
+FlipCnkModelStripWinding(NJS_CNK_MODEL* pModel, int idxStrip, int idxTri)
+{
+    sint16* plist = pModel->plist;
+    int type;
+
+    while (1)
+    {
+        while (1)
+        {
+            while (1)
+            {
+                while (1)
+                {
+                    while (1)
+                    {
+                        while (1)
+                        {
+                            type = ((uint8*)plist)[0];
+
+                            if (type >= NJD_BITSOFF)
+                                break;
+
+                            ++plist;
+                        }
+
+                        if (type >= NJD_TINYOFF)
+                            break;
+
+                        ++plist;
+                    }
+
+                    if (type >= NJD_MATOFF)
+                        break;
+
+                    plist += 2;
+                }
+
+                if (type >= NJD_VERTOFF)
+                    break;
+
+                plist += 2;
+
+                switch (type) {
+                case NJD_CM_D:
+                case NJD_CM_A:
+                case NJD_CM_S:
+                    plist += 2;
+                    break;
+                case NJD_CM_DA:
+                case NJD_CM_DS:
+                case NJD_CM_AS:
+                    plist += 4;
+                    break;
+                case NJD_CM_DAS:
+                    plist += 6;
+                    break;
+                }
+            }
+
+            if (type >= NJD_STRIPOFF)
+                break;
+        }
+
+        if (type == NJD_ENDOFF)
+            break;
+
+        if (idxStrip-- == 0)
+        {
+            int nbstrip = plist[2] & 0x3FFF;
+
+            plist += 3;
+
+            while (nbstrip--)
+            {
+                int len = *plist;
+
+                if (idxTri-- == 0)
+                {
+                    *plist = -len;
+                    return;
+                }
+
+                ++plist;
+
+                if (len < 0)
+                    len = -len;
+
+                plist += len * 3;
+            }
+
+            ModNonFatalFuncError("'idxTri' param is out of range!");
+            return;
+        }
+
+        plist += ((uint16*)plist)[1] + 2;
+    }
+
+    ModNonFatalFuncError("'idxStrip' param is out of range!");
+}
+
+void
+CnkMaterialFlagOn(Sint16* pPList, int idxMat, uint32 flag)
+{
+    Sint16* plist = pPList;
+    int type;
+
+    while (1)
+    {
+        while (1)
+        {
+            while (1)
+            {
+                while (1)
+                {
+                    while (1)
+                    {
+                        while (1)
+                        {
+                            type = ((uint8*)plist)[0];
+
+                            if (type >= NJD_BITSOFF)
+                                break;
+
+                            ++plist;
+                        }
+
+                        if (type >= NJD_TINYOFF)
+                            break;
+
+                        ++plist;
+                    }
+
+                    if (type >= NJD_MATOFF)
+                        break;
+
+                    plist += 2;
+                }
+
+                if (type >= NJD_VERTOFF)
+                    break;
+
+                plist += 2;
+
+                switch (type) {
+                case NJD_CM_D:
+                case NJD_CM_A:
+                case NJD_CM_S:
+                    plist += 2;
+                    break;
+                case NJD_CM_DA:
+                case NJD_CM_DS:
+                case NJD_CM_AS:
+                    plist += 4;
+                    break;
+                case NJD_CM_DAS:
+                    plist += 6;
+                    break;
+                }
+            }
+
+            if (type >= NJD_STRIPOFF)
+                break;
+        }
+
+        if (type == NJD_ENDOFF)
+            break;
+
+        if (idxMat == -1)
+        {
+            ((uint16*)plist)[0] |= flag;
+        }
+        else if (idxMat-- == 0)
+        {
+            ((uint16*)plist)[0] |= flag;
+            return;
+        }
+
+        plist += ((uint16*)plist)[1] + 2;
+    }
+}
+
+void
+CnkModelMaterialFlagOn(NJS_CNK_MODEL* pModel, int idxMat, uint32 flag)
+{
+    CnkMaterialFlagOn(pModel->plist, idxMat, flag);
+}
+
+void
+CnkLandTableMaterialFlagOn(OBJ_LANDTABLE* pLand, uint32 flag)
+{
+    NJS_CNK_MODEL* models[32] = {};
+
+    OBJ_LANDENTRY* plentry = pLand->pLandEntry;
+
+    const sint16 nb = pLand->ssCount;
+
+    for (int i = 0; i < nb; ++i)
+    {
+        NJS_CNK_OBJECT* object = plentry->pObject.cnk;
+
+        for (int j = 0; j < arylen(models); ++j)
+        {
+            if (models[j] == NULL)
+            {
+                models[j] = object->model;
+                CnkModelMaterialFlagOn(object->model, -1, NJD_FST_DB);
+                goto LOOP;
+            }
+
+            if (models[j] == object->model)
+                goto LOOP;
+        }
+
+        CnkModelMaterialFlagOn(object->model, -1, NJD_FST_DB); // Backup in case 'models' is full
+
+    LOOP:
+
+        ++plentry;
+    }
+}
+
+void
+CnkMaterialFlagOff(Sint16* pPList, int idxMat, uint32 flag)
+{
+    Sint16* plist = pPList;
+    int type;
+
+    while (1)
+    {
+        while (1)
+        {
+            while (1)
+            {
+                while (1)
+                {
+                    while (1)
+                    {
+                        while (1)
+                        {
+                            type = ((uint8*)plist)[0];
+
+                            if (type >= NJD_BITSOFF)
+                                break;
+
+                            ++plist;
+                        }
+
+                        if (type >= NJD_TINYOFF)
+                            break;
+
+                        ++plist;
+                    }
+
+                    if (type >= NJD_MATOFF)
+                        break;
+
+                    plist += 2;
+                }
+
+                if (type >= NJD_VERTOFF)
+                    break;
+
+                plist += 2;
+
+                switch (type) {
+                case NJD_CM_D:
+                case NJD_CM_A:
+                case NJD_CM_S:
+                    plist += 2;
+                    break;
+                case NJD_CM_DA:
+                case NJD_CM_DS:
+                case NJD_CM_AS:
+                    plist += 4;
+                    break;
+                case NJD_CM_DAS:
+                    plist += 6;
+                    break;
+                }
+            }
+
+            if (type >= NJD_STRIPOFF)
+                break;
+        }
+
+        if (type == NJD_ENDOFF)
+            break;
+
+        if (idxMat == -1)
+        {
+            ((uint16*)plist)[0] &= ~flag;
+        }
+        else if (idxMat-- == 0)
+        {
+            ((uint16*)plist)[0] &= ~flag;
+            return;
+        }
+
+        plist += ((uint16*)plist)[1] + 2;
+    }
+}
+
+void
+CnkModelMaterialFlagOff(NJS_CNK_MODEL* pModel, int idxMat, uint32 flag)
+{
+    CnkMaterialFlagOff(pModel->plist, idxMat, flag);
+}
+
+void
+CnkMaterialDiffuse(Sint16* pPList, int idxMat, int a, int r, int g, int b)
+{
+    Sint16* plist = pPList;
+    int type;
+
+    while (1)
+    {
+        while (1)
+        {
+            while (1)
+            {
+                while (1)
+                {
+                    while (1)
+                    {
+                        while (1)
+                        {
+                            type = ((uint8*)plist)[0];
+
+                            if (type >= NJD_BITSOFF)
+                                break;
+
+                            ++plist;
+                        }
+
+                        if (type >= NJD_TINYOFF)
+                            break;
+
+                        ++plist;
+                    }
+
+                    if (type >= NJD_MATOFF)
+                        break;
+
+                    plist += 2;
+                }
+
+                if (type >= NJD_VERTOFF)
+                    break;
+
+                plist += 2;
+
+                switch (type) {
+                case NJD_CM_D:
+                    if (!idxMat)
+                    {
+                        SET_COLOR:
+
+                        uint8* col = (uint8*)plist;
+
+                        if (a != -1)
+                            col[3] = (uint8)a;
+                        if (r != -1)
+                            col[2] = (uint8)r;
+                        if (g != -1)
+                            col[1] = (uint8)g;
+                        if (b != -1)
+                            col[0] = (uint8)b;
+
+                        return;
+                    }
+
+                    --idxMat;
+
+                case NJD_CM_A:
+                case NJD_CM_S:
+                    plist += 2;
+                    break;
+                case NJD_CM_DA:
+                case NJD_CM_DS:
+                    if (!idxMat)
+                        goto SET_COLOR;
+
+                    --idxMat;
+
+                case NJD_CM_AS:
+                    plist += 4;
+                    break;
+                case NJD_CM_DAS:
+                    if (!idxMat)
+                        goto SET_COLOR;
+
+                    --idxMat;
+
+                    plist += 6;
+                    break;
+                }
+            }
+
+            if (type >= NJD_STRIPOFF)
+                break;
+        }
+
+        if (type == NJD_ENDOFF)
+            break;
+
+        plist += ((uint16*)plist)[1] + 2;
+    }
+
+    ModNonFatalFuncError("'idxStrip' param is out of range!");
+}
+
+void
+CnkModelMaterialDiffuse(NJS_CNK_MODEL* pModel, int idxMat, int a, int r, int g, int b)
+{
+    CnkMaterialDiffuse(pModel->plist, idxMat, a, r, g, b);
+}
+
+void
+SwapGVMTableIndex(TEX_GVMTABLE* pGvmTable, int idx1, int idx2)
+{
+	TEX_GVMTABLE tmp = pGvmTable[idx1];
+    pGvmTable[idx1] = pGvmTable[idx2];
+    pGvmTable[idx2] = tmp;
+}
+
+void
+__ModNonFatalFuncError(const char* func, const char* body)
+{
+    char str[128];
+
+    snprintf(str, 128, "RenderFix: %s", func);
+
+    UserWarningMessageBox(str, body);
 }
 
 void
--- sa2b-render-fix/tools.h
+++ sa2b-render-fix/tools.h
@@ -1,5 +1,131 @@
 #pragma once
 
-void	SwapGVMTableIndex(int pGvmTable, int idx1, int idx2);
+#include <sa2b/memtools.h>
 
+#include <sa2b/ninja/ninja.h>
+
+/*
+*   Abstract Types
+*/
+typedef struct _OBJ_LANDTABLE   OBJ_LANDTABLE;
+
+/*
+*   Display Order Fixes
+*/
+#define DISP        0x14
+#define DISP_DELY   0x1C
+#define DISP_SORT   0x20
+#define DISP_LATE   0x24
+#define DISP_LAST   0x28
+#define DISP_SHAD   0x2C
+
+#define SwitchDisplayer(addr, disp) WriteData(addr + 2, disp, uint8) /* Use this on 'mov dword ptr' operations */
+
+/*
+*   Chunk Tri Strip Winding
+*/
+void    FlipCnkModelWinding(NJS_CNK_MODEL* pModel);
+void    FlipCnkModelStripWinding(NJS_CNK_MODEL* pModel, int idxStrip, int idxTri);
+
+/*
+*   Chunk Material Params
+*/
+/** Use '-1' to set all materials within the strip data **/
+void    CnkMaterialFlagOn(Sint16* pPList, int idxMat, uint32 flag);
+void    CnkMaterialFlagOff(Sint16* pPList, int idxMat, uint32 flag);
+
+void    CnkModelMaterialFlagOn(NJS_CNK_MODEL* pModel, int idxMat, uint32 flag);
+void    CnkModelMaterialFlagOff(NJS_CNK_MODEL* pModel, int idxMat, uint32 flag);
+
+/** Chunk LandTable Materials **/
+void    CnkLandTableMaterialFlagOn(OBJ_LANDTABLE* pLand, uint32 flag);
+void    CnkLandTableMaterialFlagOff(OBJ_LANDTABLE* pLand, uint32 flag);
+
+/** Use '-1' for params you don't wish to change, 
+    those values won't be applied at all to the model **/
+void    CnkMaterialDiffuse(Sint16* pPList, int idxMat, int a, int r, int g, int b);
+void    CnkMaterialAmbient(Sint16* pPList, int idxMat, int a, int r, int g, int b); // MIA
+void    CnkMaterialSpecular(Sint16* pPList, int idxMat, int i, int r, int g, int b);// MIA
+
+void    CnkModelMaterialDiffuse(NJS_CNK_MODEL* pModel, int idxMat, int a, int r, int g, int b);
+void    CnkModelMaterialAmbient(NJS_CNK_MODEL* pModel, int idxMat, int a, int r, int g, int b); // MIA
+void    CnkModelMaterialSpecular(NJS_CNK_MODEL* pModel, int idxMat, int i, int r, int g, int b);// MIA
+
+/*
+*   GVM Tables
+*/
+typedef struct gvmtable TEX_GVMTABLE;
+
+void	SwapGVMTableIndex(TEX_GVMTABLE* pGvmTable, int idx1, int idx2);
+
+/*
+*   Mod Tools
+*/
+void    __ModNonFatalFuncError(const char* func, const char* body);
 void	ModConflictWarning(const char* body);
+
+#define ModNonFatalFuncError(body) __ModNonFatalFuncError(__func__, body)
+
+/*
+*   Raw Pointer (ptr) Variants
+*/
+#ifdef __cplusplus
+
+/** Chunk Tri Strip Winding **/
+inline void
+FlipCnkModelWinding(ptr pModel)
+{
+    return FlipCnkModelWinding((NJS_CNK_MODEL*)pModel);
+}
+
+inline void
+FlipCnkModelStripWinding(ptr pModel, int idxStrip, int idxTri)
+{
+    return FlipCnkModelStripWinding((NJS_CNK_MODEL*)pModel, idxStrip, idxTri);
+}
+
+/** Chunk Material Params **/
+inline void
+CnkModelMaterialFlagOn(ptr pModel, int idxStrip, uint32 flag)
+{
+    return CnkModelMaterialFlagOn((NJS_CNK_MODEL*)pModel, idxStrip, flag);
+}
+
+inline void
+CnkModelMaterialFlagOff(ptr pModel, int idxStrip, uint32 flag)
+{
+    return CnkModelMaterialFlagOff((NJS_CNK_MODEL*)pModel, idxStrip, flag);
+}
+
+inline void
+CnkMaterialFlagOn(ptr pPList, int idxStrip, uint32 flag)
+{
+    return CnkMaterialFlagOn((Sint16*)pPList, idxStrip, flag);
+}
+
+inline void
+CnkMaterialFlagOff(ptr pPList, int idxStrip, uint32 flag)
+{
+    return CnkMaterialFlagOff((Sint16*)pPList, idxStrip, flag);
+}
+
+inline void 
+CnkModelMaterialDiffuse(ptr pModel, int idxMat, int a, int r, int g, int b)
+{
+    return CnkModelMaterialDiffuse((NJS_CNK_MODEL*)pModel, idxMat, a, r, g, b);
+}
+
+inline void
+CnkMaterialDiffuse(ptr pPList, int idxMat, int a, int r, int g, int b)
+{
+    return CnkMaterialDiffuse((Sint16*)pPList, idxMat, a, r, g, b);
+}
+
+/** GVM Tables **/
+inline void
+SwapGVMTableIndex(ptr pGvmTable, int idx1, int idx2)
+{
+    return SwapGVMTableIndex((TEX_GVMTABLE*)pGvmTable, idx1, idx2);
+}
+
+#endif /* __cplusplus */
