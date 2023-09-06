0117cf535a560f5953dc78fa977dac1d13070364
Shaddatic <Shaddatic@gmail.com>
2023-09-06 06:15:42 +0100

    Added 'Fix Particle Scale'

--- sa2b-render-fix/render-fix.cpp
+++ sa2b-render-fix/render-fix.cpp
@@ -193,6 +193,56 @@ Init(const char* path, const HelperFunctions* pHelpFunc)
         EnableBackfaceCulling();
 	}
 
+    if (ConfigGetInt(conf, "main", "ptcl_scl", 1))
+    {
+        static const double ptcldbl00 = 1.5;
+        static const double ptcldbl01 = 2.0;
+
+        static const double ptcldbl02 = 1.0;
+
+        static const double ptcldbl03 = 30.0;
+        static const double ptcldbl04 = 32.5;
+
+        static const double ptcldbl05 = 0.0000457763671875;
+
+        /** Some particles were made 2x as big to counteract 
+            the halvaning, this reverts that **/
+
+#define ReplaceFloat(addr, pflt) WritePointer(addr + 2, pflt)
+
+        ReplaceFloat(0x006D6117, &ptcldbl00); // RocketRocket
+        ReplaceFloat(0x006D611D, &ptcldbl01); // ^
+
+        ReplaceFloat(0x006D5455, &ptcldbl02); // Rocket
+        ReplaceFloat(0x006D545B, &ptcldbl01); // ^
+
+        ReplaceFloat(0x006F4FC3, &ptcldbl03); // MH Rocket
+        ReplaceFloat(0x006F4FC9, &ptcldbl04);
+        ReplaceFloat(0x006F500C, &ptcldbl03); // ^
+        ReplaceFloat(0x006F5012, &ptcldbl04);
+        ReplaceFloat(0x006F50C9, &ptcldbl03); // ^
+        ReplaceFloat(0x006F50CF, &ptcldbl04);
+
+        WriteData(0x006CF22E, 0x9090, uint16);// RocketMissile
+        ReplaceFloat(0x006CF228, &ptcldbl05); // ^
+        ReplaceFloat(0x006CF230, &ptcldbl01); // ^
+        WriteData(0x006CEEF2, 0x9090, uint16);// ^
+        ReplaceFloat(0x006CEEE8, &ptcldbl05); // ^
+        ReplaceFloat(0x006CEEF4, &ptcldbl01); // ^
+
+        ReplaceFloat(0x004B1290, &ptcldbl00); // StartRocketRocket (Final Rush)
+        ReplaceFloat(0x004B1296, &ptcldbl01);
+
+        WriteNoOP(0x00492506, 0x0049250C); // ptcl * 0.5
+
+        /** Now just the polygon-particles **/
+
+        static double ptclpolyscl;
+
+        ptclpolyscl = (8.0 * GetDisplayRatioY());
+        ReplaceFloat(0x007801A6, &ptclpolyscl);
+    }
+
 	int modeltint = ConfigGetInt(conf, "main", "model_tint", 2);
 
 	if (modeltint)
