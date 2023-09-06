2f1894d2c079dea7c68772e7b5218d2a6fa9ab45
Shaddatic <Shaddatic@gmail.com>
2023-09-06 06:06:14 +0100

    Added 'Restore Lighting' to Boom Boo section

--- sa2b-render-fix/boss-bogy.cpp
+++ sa2b-render-fix/boss-bogy.cpp
@@ -1,9 +1,17 @@
 #include <sa2b/core.h>
 #include <sa2b/config.h>
 
+#include <sa2b/src/light.h>
+
 #include <tools.h>
 
-#define SECT "bossbogy"
+static LIGHT lights_bigbogy[] =
+{
+    { { 0.0f, 0.707f, 0.707f }, 1.0f, 0.5f, 1.0f, 1.0f, 1.0f },
+    { { 0.0f, 0.707f, 0.707f }, 0.3f, 0.6f, 1.0f, 1.0f, 1.0f },
+    { { 0.0f, 0.707f, 0.707f }, 0.5f, 0.8f, 1.0f, 1.0f, 1.0f },
+    { { 0.0f, 0.707f, 0.707f }, 0.0f, 1.0f, 1.0f, 0.5f, 0.5f }
+};
 
 static void
 SunDoorsFix()
@@ -18,4 +26,8 @@ BossBogySettings(const config* conf)
 	{
         SwapGVMTableIndex(0x0103622C, 2, 4);
 	}
+
+    if (ConfigGetInt(conf, SECT, "lights", 1))
+    {
+        memcpy((void*)0x01036738, lights_bigbogy, sizeof(lights_bigbogy));
 }
