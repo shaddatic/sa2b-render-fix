896654236d99b4475946906fe346e3deafd884bc
Shaddatic <Shaddatic@gmail.com>
2024-02-25 17:29:50 +0000

    Update SAMT

--- include/sa2b/modloader.h
+++ include/sa2b/modloader.h
@@ -15,25 +15,22 @@
 /************************/
 /*  Mod Loader Version  */
 /************************/
-#define MODLOADER_VER      (11)
+#define MODLOADER_VER      (12)
 
 /************************/
 /*  Functions           */
 /************************/
 EXTERN_START
+/****** Version >= 0 ********************************************************/
 /** Get installed mod-loader version **/
 int32_t     ML_GetVersion(void);
 
-/*
-*   Mod-Loader Version >= 4
-*/
+/****** Version >= 4 ********************************************************/
 /** Get current save paths **/
 const char* ML_GetMainSavePath(void);
 const char* ML_GetChaoSavePath(void);
 
-/*
-*   Mod-Loader Version >= 7
-*/
+/****** Version >= 7 ********************************************************/
 /** Returns active, replaced gd_PC path for given file **/
 const char* ML_GetReplaceablePath(const char* path);
 
@@ -43,9 +40,7 @@ void        ML_ReplaceFile(const char* path, const char* pathNew);
 /** Change title of SA2's main window **/
 void        ML_SetWindowTitle(const wchar_t* wTitle);
 
-/*
-*   Mod-Loader Version >= 8
-*/
+/****** Version >= 8 ********************************************************/
 /** Set scale and color of ML's debug text font **/
 void        ML_SetDebugFontScale(float scale);
 void        ML_SetDebugFontColor(uint32_t color);
@@ -55,17 +50,17 @@ void        ML_DisplayDebugString(int njmLocation, const char* string);
 void        ML_DisplayDebugStringF(int njmLocation, char* buffer, const char* format, ...);
 void        ML_DisplayDebugInt(int njmLocation, int val, int nbDigits);
 
-/*
-*   Mod-Loader Version >= 10
-*/
+/****** Version >= 10 *******************************************************/
 /** Register new character voice ADX file, returns ID of new voice line **/
 uint16_t    ML_RegisterVoice(const char* fileJP, const char* fileEN);
 
-/*
-*   Mod-Loader Version >= 11
-*/
+/****** Version >= 11 *******************************************************/
 /** Replace and swap textures inside a texture file in real time, 'file_path' must point to a valid image file (eg. PNG) **/
 void        ML_ReplaceTexture(const char* prs_name, const char* tex_name, const char* file_path, uint32_t gbix, uint32_t width, uint32_t height);
+
+/****** Version >= 12 *******************************************************/
+/** Reset given, replaceable file path to the default game directory **/
+void        ML_ResetReplaceablePath(const char* fpath);
 
 EXTERN_END
 
