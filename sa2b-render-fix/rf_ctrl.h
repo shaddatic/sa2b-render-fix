0fddd8c9a180ee3693805ab7d9fcbb8d49d5b056
Shaddatic <Shaddatic@gmail.com>
2024-04-04 00:53:21 +0100

    Add Render Fix Control header

---
+++ sa2b-render-fix/rf_ctrl.h
@@ -0,0 +1,21 @@
+/*
+*   SA2 Render Fix - '/rf_ctrl.h'
+*
+*   Description:
+*       Render Fix control functions.
+*
+*   Contributors:
+*   -   Shaddatic
+*/
+#ifndef _RF_CTRL_H_
+#define _RF_CTRL_H_
+
+/************************/
+/*  Functions           */
+/************************/
+EXTERN_START
+void    RFCTRL_CheapShadowChaoWorldDisable( void ); /* Don't apply cheap shadows in Chao World */
+
+EXTERN_END
+
+#endif/*_RF_CULL_H_*/
