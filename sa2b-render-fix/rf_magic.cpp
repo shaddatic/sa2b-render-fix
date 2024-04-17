e641c84024278af1371de3345681dcbf740f135b
Shaddatic <Shaddatic@gmail.com>
2024-04-17 21:26:39 +0100

    Added module specifically for calling Magic functions

---
+++ sa2b-render-fix/rf_magic.cpp
@@ -0,0 +1,38 @@
+#include <sa2b/core.h>
+
+/** Magic **/
+#include <sa2b/magic/magic.h>
+
+/** Self **/
+#include <rf_magic.h>
+
+/** Enum def **/
+using enum Magic::RenderCore::RenderDevice::RenderState;
+
+EXTERN
+void
+RF_MagicSetAlphaRef(uint32_t value)
+{
+    g_pRenderDevice->vft->SetRenderState(g_pRenderDevice, RS_ALPHA_REF, value);
+}
+
+EXTERN
+void
+RF_MagicSetAlphaTestFunc(RFE_MAGIC_CMP func)
+{
+    g_pRenderDevice->vft->SetRenderState(g_pRenderDevice, RS_ALPHA_TEST_FUNC, func);
+}
+
+EXTERN
+void
+RF_MagicSetShaderConstantVec4(RFE_MAGIC_SHADER type, int32_t reg, float x, float y, float z, float w)
+{
+    const Magic::MATH::Vector4<float> vec4 = {
+        .x = x,
+        .y = y,
+        .z = z,
+        .w = w
+    };
+
+    g_pRenderDevice->vft->ShaderSetConstant9(g_pRenderDevice, type, reg, &vec4);
+}
---
+++ sa2b-render-fix/rf_magic.h
@@ -0,0 +1,49 @@
+/*
+*   SA2 Render Fix - '/rf_magic.h'
+*
+*   Description:
+*       Wrapper functions for Magic's C++ interface.
+*
+*   Contributors:
+*   -   Shaddatic
+*/
+#ifndef _RF_MAGIC_H_
+#define _RF_MAGIC_H_
+
+/************************/
+/*  Enums               */
+/************************/
+typedef enum
+{
+    MAGIC_CMP_NEVER,
+    MAGIC_CMP_LESS,
+    MAGIC_CMP_EQUAL,
+    MAGIC_CMP_LESSEQUAL,
+    MAGIC_CMP_GREATER,
+    MAGIC_CMP_NOTEQUAL,
+    MAGIC_CMP_GREATEREQUAL,
+    MAGIC_CMP_ALWAYS,
+}
+RFE_MAGIC_CMP;
+
+typedef enum
+{
+    MAGIC_SHADER_VERTEX,
+    MAGIC_SHADER_PIXEL,
+}
+RFE_MAGIC_SHADER;
+
+/************************/
+/*  Functions           */
+/************************/
+EXTERN_START
+/** Set alpha test params **/
+void    RF_MagicSetAlphaRef(uint32_t value);
+void    RF_MagicSetAlphaTestFunc(RFE_MAGIC_CMP func);
+
+/** Set shader constant using a vector4 **/
+void    RF_MagicSetShaderConstantVec4( RFE_MAGIC_SHADER type, int32_t reg, float x, float y, float z, float w );
+
+EXTERN_END
+
+#endif/*_RF_MAGIC_H_*/
