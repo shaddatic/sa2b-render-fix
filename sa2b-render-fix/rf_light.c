4325feaeb786959d3272726b7db0d5b4f45a6fbd
Shaddatic <shaddatic@proton.me>
2025-05-24 12:01:52 +0100

    Rewrite light data system for Render Fix, disconnecting it from GX/Ginja. Also adding support for light intensity emulation.
    This is a code breaking change, and will take me a bit to get everything else updated.

--- sa2b-render-fix/rf_light.c
+++ sa2b-render-fix/rf_light.c
@@ -25,7 +25,7 @@
 /*  Constants           */
 /************************/
 /****** Max Light Index *************************************************************/
-#define MAX_LIGHT               (4) /* shader only supports 4 lights                */
+#define RXD_LIGHT_MAX           (4) /* gx shader only supports 4 lights             */
 
 /************************/
 /*  Structures          */
@@ -39,8 +39,8 @@ RX_COLOR;
 
 typedef struct
 {
-    NJS_VECTOR vec;
     NJS_POINT3 pos;
+    NJS_VECTOR vec;
     RX_COLOR   color;
 
     f32        angle_const;
@@ -65,20 +65,286 @@ RX_LIGHT;
 /*  Export Data         */
 /************************/
 /****** Global Ambient Color ********************************************************/
-NJS_ARGB _rj_ambi_color_;
+RJS_AMBIENT _rj_cnk_light_ambient_;
+
+/****** Global Lights ***************************************************************/
+RJS_LIGHT _rj_cnk_light_[6];
 
 /************************/
 /*  Game Data           */
 /************************/
-/****** Global Ambient Color ********************************************************/
+/****** GX Lights *******************************************************************/
 #define _gj_light_list_         DATA_ARY(RX_LIGHT, 0x025F0120, [5])
 
 /************************/
 /*  Source              */
 /************************/
+/****** Usercall ********************************************************************/
+static void
+GX_LoadLightObjImm(const RX_LIGHT_ATTR* pAttr, int num)
+{
+    static const void* const GX_LoadLightObjImm_p = (void*)0x004240D0;
+
+    __asm
+    {
+        mov ecx, [num]
+        mov eax, [pAttr]
+        call GX_LoadLightObjImm_p
+}
+}
+
+static void
+rjLoadGxLight(Int light)
+{
+    GX_LoadLightObjImm(_gj_light_list_[light].pAttr, light);
+}
+
+/****** Static **********************************************************************/
+static void
+___rjCnkSetLightVector(Int light, Float vx, Float vy, Float vz)
+{
+    RJS_LIGHT* p_lite = &_rj_cnk_light_[light];
+
+    p_lite->type = RJD_CNK_LIGHTMD_DIR;
+
+    p_lite->v.x = -vx;
+    p_lite->v.y = -vy;
+    p_lite->v.z = -vz;
+}
+
+static void
+___rjCnkSetLightSwitch(Int light, Int flag)
+{
+    if ( flag )
+    {
+        _rj_cnk_light_switch_ |= ( 1<<light );
+    }
+    else
+    {
+        _rj_cnk_light_switch_ &= ~( 1<<light );
+    }
+}
+
+static void
+___rjCnkSetLightColor(Int light, Float lr, Float lg, Float lb)
+{
+    RJS_LIGHT* p_lite = &_rj_cnk_light_[light];
+
+    p_lite->r = lr;
+    p_lite->g = lg;
+    p_lite->b = lb;
+
+    p_lite->inten = 1.f;
+}
+
+static void
+___rjCnkSetLightIntensity(Int light, Float inten, Float ambient)
+{
+    RJS_LIGHT* p_lite = &_rj_cnk_light_[light];
+
+    p_lite->inten = CLAMP(inten, 0.f, 1.f);
+
+    if (ambient >= 0.f)
+    {
+        const Float ambi = MIN(ambient, 1.f);
+
+        _rj_cnk_light_ambient_.r = p_lite->r * ambi;
+        _rj_cnk_light_ambient_.g = p_lite->g * ambi;
+        _rj_cnk_light_ambient_.b = p_lite->b * ambi;
+
+        _rj_cnk_light_ambient_.inten = ambi;
+}
+}
+
+static void
+___rjCnkSetLightMatrix(Int light)
+{
+    RJS_LIGHT* p_lite = &_rj_cnk_light_[light];
+
+    switch ( p_lite->type )
+    {
+        case RJD_CNK_LIGHTMD_DIR:
+        {
+            njCalcVector(NULL, &p_lite->v, &p_lite->v);
+            break;
+        }
+        case RJD_CNK_LIGHTMD_POINT:
+        {
+            njCalcPoint(NULL, &p_lite->p, &p_lite->p);
+            break;
+        }
+        case RJD_CNK_LIGHTMD_SPOT:
+        {
+            njCalcVector(NULL, &p_lite->v, &p_lite->v);
+            njCalcPoint( NULL, &p_lite->p, &p_lite->p);
+            break;
+        }
+    }
+}
+
+static void
+___rjCnkPushLightToGX(Int light)
+{
+    if (light >= RXD_LIGHT_MAX)
+    {
+        return;
+    }
+
+    if ( _rj_cnk_light_switch_ & ( 1 << light ) )
+    {
+        const RJS_LIGHT* restrict p_lite = &_rj_cnk_light_[light];
+
+        RX_LIGHT_ATTR* const p_gx_lite = _gj_light_list_[light].pAttr;
+
+        p_gx_lite->color.r = p_lite->r * p_lite->inten;
+        p_gx_lite->color.g = p_lite->g * p_lite->inten;
+        p_gx_lite->color.b = p_lite->b * p_lite->inten;
+
+        p_gx_lite->angle_const  = 0.f;
+        p_gx_lite->angle_linear = 0.f;
+        p_gx_lite->angle_quad   = 0.f;
+        p_gx_lite->angle        = 90.f;
+
+        p_gx_lite->atten_const  =  4.f;
+        p_gx_lite->atten_linear =  0.f;
+        p_gx_lite->atten_quad   = -3.f;
+
+        p_gx_lite->pos.x = p_lite->v.x * 100000.f;
+        p_gx_lite->pos.y = p_lite->v.y * 100000.f;
+        p_gx_lite->pos.z = p_lite->v.z * 100000.f;
+
+        p_gx_lite->vec = (NJS_VECTOR){ 0 };
+
+        gjSetLightSwitch(light, TRUE);
+    }
+    else
+    {
+        gjSetLightSwitch(light, FALSE);
+    }
+
+    rjLoadGxLight(light);
+}
+
 /****** Extern **********************************************************************/
 void
-rjSetLightColor(int id, Float r, Float g, Float b)
+rjCnkSetLightSwitch(Int light, Int flag)
+{
+    if ( light == RJD_CNK_LIGHT_ALL )
+    {
+        for ( int i = 0; i < RJD_CNK_LIGHT_NUM; ++i )
+        {
+            ___rjCnkSetLightSwitch(i, flag);
+        }
+    }
+    else
+    {
+        ___rjCnkSetLightSwitch(light, flag);
+    }
+}
+
+void
+rjCnkSetAmbient(Float ar, Float ag, Float ab)
+{
+    _rj_cnk_light_ambient_.r = ar;
+    _rj_cnk_light_ambient_.g = ag;
+    _rj_cnk_light_ambient_.b = ab;
+
+    /** ambient intensity is set to the largest color value, as it's
+        the best approximation that can be made **/
+
+    _rj_cnk_light_ambient_.inten = MAX( MAX( ar, ag ), ab );
+}
+
+void
+rjCnkSetLightColor(Int light, Float lr, Float lg, Float lb)
+{
+    if ( light == RJD_CNK_LIGHT_ALL )
+    {
+        for ( int i = 0; i < RJD_CNK_LIGHT_NUM; ++i )
+        {
+            ___rjCnkSetLightColor(i, lr, lg, lb);
+        }
+    }
+    else
+    {
+        ___rjCnkSetLightColor(light, lr, lg, lb);
+    }
+}
+
+void
+rjCnkSetLightIntensity(Int light, Float inten, Float ambient)
+{
+    if ( light == RJD_CNK_LIGHT_ALL )
+    {
+        for ( int i = 0; i < RJD_CNK_LIGHT_NUM; ++i )
+        {
+            ___rjCnkSetLightIntensity(i, inten, -1.f);
+}
+    }
+    else
+    {
+        ___rjCnkSetLightIntensity(light, inten, ambient);
+    }
+}
+
+void
+rjCnkSetLightVector(Int light, Float vx, Float vy, Float vz)
+{
+    if ( light == RJD_CNK_LIGHT_ALL )
+    {
+        for ( int i = 0; i < RJD_CNK_LIGHT_NUM; ++i )
+        {
+            ___rjCnkSetLightVector(i, vx, vy, vz);
+        }
+    }
+    else
+    {
+        ___rjCnkSetLightVector(light, vx, vy, vz);
+    }
+}
+
+void
+rjCnkSetLightMatrix(Int light)
+{
+    if ( light == RJD_CNK_LIGHT_ALL )
+    {
+        for ( int i = 0; i < RJD_CNK_LIGHT_NUM; ++i )
+        {
+            ___rjCnkSetLightMatrix(i);
+    }
+}
+    else
+    {
+        ___rjCnkSetLightMatrix(light);
+    }
+}
+
+/****** Rj To Gx ********************************************************************/
+void
+rjCnkPushLightToGX(Int light)
+{
+    if ( light == RJD_CNK_LIGHT_ALL ) // all lights
+    {
+        for ( int i = 0; i < RJD_CNK_LIGHT_NUM; ++i )
+        {
+            ___rjCnkPushLightToGX(i);
+        }
+    }
+    else
+    {
+        ___rjCnkPushLightToGX(light);
+    }
+}
+
+void
+rjCnkPushAmbientToGX(void)
+{
+    RX_SetChanAmbColor_Direct(_rj_cnk_light_ambient_.r, _rj_cnk_light_ambient_.g, _rj_cnk_light_ambient_.b);
+    }
+
+/****** Hooks ***********************************************************************/
+static void
+gjSetLightColor_Hook(int id, Float r, Float g, Float b)
 {
     _gj_light_list_[id].pAttr->color.a = 0.f;
     _gj_light_list_[id].pAttr->color.r = r;
@@ -86,103 +352,74 @@ rjSetLightColor(int id, Float r, Float g, Float b)
     _gj_light_list_[id].pAttr->color.b = b;
 }
 
-void
-rjSetAmbient(Float ar, Float ag, Float ab)
+static void
+gjSetAmbient_Hook(Float ar, Float ag, Float ab)
 {
-    _rj_ambi_color_.r = ar;
-    _rj_ambi_color_.g = ag;
-    _rj_ambi_color_.b = ab;
+    _rj_cnk_light_ambient_.r = ar;
+    _rj_cnk_light_ambient_.g = ag;
+    _rj_cnk_light_ambient_.b = ab;
+
+    _rj_cnk_light_ambient_.inten = MAX( MAX( ar, ag ), ab );
 
     RX_SetChanAmbColor_Direct(ar, ag, ab);
 }
 
 static void
-SetLightVector(Int light, Float vx, Float vy, Float vz)
+gjLoadLight_Hook(Int light)
 {
-    RX_LIGHT_ATTR* const litep = _gj_light_list_[light].pAttr;
-
-    litep->angle_const  = 0.f;
-    litep->angle_linear = 0.f;
-    litep->angle_quad   = 0.f;
-    litep->angle        = 90.f;
-
-    litep->atten_const  =  4.f;
-    litep->atten_linear =  0.f;
-    litep->atten_quad   = -3.f;
-
-    litep->vec.x = vx * -100000.0f;
-    litep->vec.y = vy * -100000.0f;
-    litep->vec.z = vz * -100000.0f;
-
-    litep->pos = (NJS_VECTOR){ 0 };
-}
-
-void
-rjCnkSetLightVectorEx(Int light, Float vx, Float vy, Float vz)
-{
-    if (light == RFD_CNK_MULTILIGHT_ALL) // all lights
+    //if ( light != 0 )
     {
-        for (int i = 0; i < MAX_LIGHT; ++i)
+        const RX_LIGHT_ATTR* p_attr = _gj_light_list_[light].pAttr;
+
+        RJS_LIGHT* p_lite = &_rj_cnk_light_[light];
+
+        p_lite->r = p_attr->color.r;
+        p_lite->g = p_attr->color.g;
+        p_lite->b = p_attr->color.b;
+
+        p_lite->inten = 1.f;
+
+        if ( p_attr->vec.x != 0.f || p_attr->vec.y != 0.f || p_attr->vec.z != 0.f )
         {
-            SetLightVector(i, vx, vy, vz);
+            p_lite->type = RJD_CNK_LIGHTMD_SPOT;
+
+            p_lite->v.x = -p_attr->vec.x;
+            p_lite->v.y = -p_attr->vec.y;
+            p_lite->v.z = -p_attr->vec.z;
+
+            p_lite->p = p_attr->pos;
+
+            p_lite->near = 0.f;
+            p_lite->far  = 0.f;
+
+            p_lite->angmin = 0.f;
+            p_lite->angmax = acosf(p_attr->angle) * -16.837942f;
+        }
+        else
+        {
+            p_lite->type = RJD_CNK_LIGHTMD_DIR;
+
+            NJS_VECTOR vec = p_attr->pos;
+
+            njUnitVector(&vec);
+
+            p_lite->v = vec;
         }
     }
-    else if (light <= MAX_LIGHT)
-    {
-        SetLightVector(light - 1, vx, vy, vz);
-    }
+
+    GX_LoadLightObjImm(_gj_light_list_[light].pAttr, light);
 }
 
-void
-rjCnkSetLightColor(Int light, Float lr, Float lg, Float lb)
+__declspec(naked)
+static void
+___gjLoadLight_Hook(void)
 {
-    if (light == RFD_CNK_MULTILIGHT_ALL) // all lights
+    __asm
     {
-        for (int i = 0; i < MAX_LIGHT; ++i)
-        {
-            rjSetLightColor(i, lr, lg, lb);
-        }
-    }
-    else if (light <= MAX_LIGHT)
-    {
-        rjSetLightColor(light - 1, lr, lg, lb);
-    }
-}
-
-void
-rjCnkSetAmbient(Float ar, Float ag, Float ab)
-{
-    rjSetAmbient(ar, ag, ab);
-}
-
-void
-rjCnkSetLightSwitch(Int light, Int flag)
-{
-    if (light == RFD_CNK_MULTILIGHT_ALL) // all lights
-    {
-        for (int i = 0; i < MAX_LIGHT; ++i)
-        {
-            gjSetLightSwitch(i, flag);
-            gjLoadLight(i);
-        }
-    }
-    else if (light <= MAX_LIGHT)
-    {
-        light--;
-
-        gjSetLightSwitch(light, flag);
-        gjLoadLight(light);
-    }
-}
-
-void
-rjCnkSetLightMatrices(void)
-{
-    for (int i = 0; i < MAX_LIGHT; ++i)
-    {
-        GXS_LIGHT_ATTR* const litep = _gj_lights_[i].pAttr;
-
-        njCalcVector(NULL, &litep->vec, &litep->vec);
+        push eax
+        call gjLoadLight_Hook
+        pop eax
+        retn
     }
 }
 
@@ -191,6 +428,8 @@ void
 RF_LightInit(void)
 {
     /** Replace SetLight and SetAmbient functions **/
-    WriteJump(0x0042A8B0, rjSetAmbient);
-    WriteJump(0x0042A950, rjSetLightColor);
+    WriteJump(0x0042A8B0, gjSetAmbient_Hook);
+    WriteJump(0x0042A950, gjSetLightColor_Hook);
+
+    WriteJump(0x0042AA60, ___gjLoadLight_Hook);
 }
--- sa2b-render-fix/rf_light.h
+++ sa2b-render-fix/rf_light.h
@@ -2,54 +2,215 @@
 *   SA2 Render Fix - '/rf_light.h'
 *
 *   Description:
-*       Functions for easily manipulating Ginja's light system.
+*     Render Fix's light data and control module.
 *
 *   Contributors:
-*   -   Shaddatic
+*     - Shaddatic
 */
-#ifndef _RF_LIGHT_H_
-#define _RF_LIGHT_H_
+#ifndef H_RF_LIGHT
+#define H_RF_LIGHT
 
 /************************/
 /*  Includes            */
 /************************/
-#include <samt/ninja/njcommon.h>
+/****** Ninja ***********************************************************************/
+#include <samt/ninja/njcommon.h>        /* ninja common                             */
 
 EXTERN_START
 
 /************************/
 /*  Constants           */
 /************************/
-#define	RFD_CNK_MULTILIGHT_1            (1) /* light 1                              */
-#define	RFD_CNK_MULTILIGHT_2            (2) /* light 2                              */
-#define	RFD_CNK_MULTILIGHT_3            (3) /* light 3                              */
-#define	RFD_CNK_MULTILIGHT_4            (4) /* light 4                              */
-#define	RFD_CNK_MULTILIGHT_5            (5) /* unimplimented                        */
-#define	RFD_CNK_MULTILIGHT_6            (6) /* unimplimented                        */
+/****** Light Indeces ***************************************************************/
+#define	RJD_CNK_LIGHT_ALL          (-1) /* all lights                               */
+#define	RJD_CNK_LIGHT_NUM           (6) /* max lights                               */
 
-#define	RFD_CNK_MULTILIGHT_ALL          (0) /* all lights                           */
+#define	RJD_CNK_LIGHT_1             (0) /* light 1                                  */
+#define	RJD_CNK_LIGHT_2             (1) /* light 2                                  */
+#define	RJD_CNK_LIGHT_3             (2) /* light 3                                  */
+#define	RJD_CNK_LIGHT_4             (3) /* light 4                                  */
+#define	RJD_CNK_LIGHT_5             (4) /* light 5                                  */
+#define	RJD_CNK_LIGHT_6             (5) /* light 6                                  */
+
+/****** Switch Flags ****************************************************************/
+#define RJD_CNK_LIGHTSW_1        (1<<0) /* light 1 is active                        */
+#define RJD_CNK_LIGHTSW_2        (1<<1) /* light 2 is active                        */
+#define RJD_CNK_LIGHTSW_3        (1<<2) /* light 3 is active                        */
+#define RJD_CNK_LIGHTSW_4        (1<<3) /* light 4 is active                        */
+#define RJD_CNK_LIGHTSW_5        (1<<4) /* light 5 is active                        */
+#define RJD_CNK_LIGHTSW_6        (1<<5) /* light 6 is active                        */
+
+/****** Light Type ******************************************************************/
+#define RJD_CNK_LIGHTMD_DIR         (0) /* directional light                        */
+#define RJD_CNK_LIGHTMD_POINT       (1) /* point light                              */
+#define RJD_CNK_LIGHTMD_SPOT        (2) /* spot light                      (GX emu) */
+
+/************************/
+/*  Structures          */
+/************************/
+/****** Rj Light ********************************************************************/
+typedef struct
+{
+    s32         type;           /* light type                                       */
+
+    /** Directional Light **/
+
+    NJS_VECTOR  v;              /* light vector                                     */
+
+    Float       r,g,b;          /* color                                            */
+    Float       inten;          /* light intensity                                  */
+
+    /** Point Light **/
+
+    NJS_POINT3 p;               /* world position                                   */
+
+    f32        near;            /* near light plane                                 */
+    f32        far;             /* far light plane                                  */
+
+    /** Spot Light **/
+
+    f32        angmin;          /* minimum angle                                    */
+    f32        angmax;          /* maximum angle                                    */
+}
+RJS_LIGHT;
+
+/****** Ambient *********************************************************************/
+typedef struct
+{
+    Float       r,g,b;          /* color                                            */
+    Float       inten;          /* ambient intensity                                */
+}
+RJS_AMBIENT;
+
+/************************/
+/*  Extern Data         */
+/************************/
+/****** Light Switch ****************************************************************/
+#define _rj_cnk_light_switch_               DATA_REF(Uint32, 0x026702E0)
+
+/****** Light Data ******************************************************************/
+EXTERN RJS_LIGHT   _rj_cnk_light_[6];       /* lights data                          */
+EXTERN RJS_AMBIENT _rj_cnk_light_ambient_;  /* ambient data                         */
 
 /************************/
 /*  Functions           */
 /************************/
-/** Init Light module **/
+/****** Init ************************************************************************/
+/*
+*   Description:
+*     Init light control module.
+*/
 void    RF_LightInit( void );
 
-/** Set Ginja light to global **/
-void    rjCnkSetLightVectorEx( Int light, Float vx, Float vy, Float vz );
-
-/** Set light color **/
-void    rjCnkSetLightColor( Int light, Float r, Float g, Float b );
-
-/** Set light ambient color **/
-void    rjCnkSetAmbient( Float r, Float g, Float b );
-
-/** Turn light on and off **/
+/****** Light Param *****************************************************************/
+/*
+*   Description:
+*     Switch a Chunk light on or off.
+*
+*   Parameters:
+*     - light       : chunk light                                 [RJD_CNK_LIGHT_#]
+*     - flag        : on/off flag                                          [ON/OFF]
+*/
 void    rjCnkSetLightSwitch( Int light, Int flag );
+/*
+*   Description:
+*     Set a Chunk light color.
+*
+*   Notes:
+*     - Internal light intensity is set to '1.f' when this function is called. To
+*       set a custom intensity, use 'SetLightIntensity'.
+*
+*   Parameters:
+*     - light       : chunk light                                 [RJD_CNK_LIGHT_#]
+*     - lr,lg,lb    : color                                               [0.f~1.f]
+*/
+void    rjCnkSetLightColor( Int light, Float lr, Float lg, Float lb );
+/*
+*   Description:
+*     Set the Chunk ambient light color.
+*
+*   Notes:
+*     - Ambient is set here OR via intensity, but can't be both.
+*     - Internal ambient intensity value is set to the largest color component
+*       value when this function is called.
+*
+*   Parameters:
+*     - ar,ag,ab    : color                                               [0.f~1.f]
+*/
+void    rjCnkSetAmbient( Float ar, Float ag, Float ab );
+/*
+*   Description:
+*     Set a Chunk light intensity, and optionally the ambient intensity.
+*
+*   Notes:
+*     - This will overwrite the ambient light color if ambient intensity is set.
+*     - Ambient is not updated when 'LIGHT_ALL' is used.
+*
+*   Parameters:
+*     - light       : chunk light                                 [RJD_CNK_LIGHT_#]
+*     - inten       : light intensity                                     [0.f~1.f]
+*     - ambient     : ambient intensity                        [opt: -1.f, 0.f~1.f]
+*/
+void    rjCnkSetLightIntensity( Int light, Float inten, Float ambient );
 
-/** Set light vectors to reflect current matrix **/
-void    rjCnkSetLightMatrices( void );
+/****** Light Modes *****************************************************************/
+/*
+*   Description:
+*     Set a Chunk light vector, and set the light mode to directional light.
+*
+*   Notes:
+*     - Vector is not normalized in-function.
+*
+*   Parameters:
+*     - light       : chunk light                                 [RJD_CNK_LIGHT_#]
+*     - vx,vy,vz    : vector
+*/
+void    rjCnkSetLightVector( Int light, Float vx, Float vy, Float vz );
+/*
+*   Description:
+*     Set a Chunk light position, and set the light mode to point light.
+*
+*   Parameters:
+*     - light       : chunk light                                 [RJD_CNK_LIGHT_#]
+*     - px,py,pz    : vector
+*/
+void    rjCnkSetLightPoint( Int light, Float px, Float py, Float pz );
+/*
+*   Description:
+*     Set Chunk point light near and far ranges.
+*
+*   Notes:
+*     - Anything closer than near is clamped to max intensity, then intensity falls
+*       off linearly until the far range.
+*
+*   Parameters:
+*     - light       : chunk light                                 [RJD_CNK_LIGHT_#]
+*/
+void    rjCnkSetLightRange( Int light, Float nrange, Float frange );
+/*
+*   Description:
+*     Set Chunk light vectors/points to the current matrix, if it hasn't been done.
+*s
+*   Parameters:
+*     - light       : chunk light                                 [RJD_CNK_LIGHT_#]
+*/
+void    rjCnkSetLightMatrix( Int light );
+
+/****** Sync GX *********************************************************************/
+/*
+*   Description:
+*     Sync light data with the game's vanilla GX light system.
+*
+*   Parameters:
+*     - light       : chunk light                               [RJD_CNK_LIGHT_1~4]
+*/
+void    rjCnkPushLightToGX( Int light );
+/*
+*   Description:
+*     Sync ambient light color data with the game's vanilla GX light system.
+*/
+void    rjCnkPushAmbientToGX( void );
 
 EXTERN_END
 
-#endif/*_RF_LIGHT_H_*/
+#endif/*H_RF_LIGHT*/
