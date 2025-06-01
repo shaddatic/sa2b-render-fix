7571fcc9287a9546a9660c2eb78cf2ac4210c0f4
Shaddatic <shaddatic@proton.me>
2025-06-01 13:37:57 +0100

    Add light files for the cart stages, and add code to load them. This overrides the game's exe light data it normally applies, so light files with the same data have also been added (though they use the Dreamcast lights, which are slightly different)

--- sa2b-render-fix/rfm_cart.c
+++ sa2b-render-fix/rfm_cart.c
@@ -1,9 +1,14 @@
 #include <samt/core.h>
 #include <samt/writemem.h>
 #include <samt/writeop.h>
+#include <samt/string.h>
 
 /** Ninja **/
 #include <samt/ninja/ninja.h>
+
+/** Game **/
+#include <samt/sonic/game.h>
+#include <samt/sonic/light.h>
 
 /** Render Fix **/
 #include <rf_core.h>
@@ -23,6 +28,16 @@ DrawCartSea(void)
     njFogEnable();
 }
 
+static void
+LoadCartLightFiles(void)
+{
+    c8 ubuf[32];
+
+    mtStrFormat(ubuf, 32, "stg%02i_light.bin", ssStageNumber);
+
+    LoadLightFile(ubuf);
+}
+
 void
 RFM_CartInit(void)
 {
@@ -36,17 +51,20 @@ RFM_CartInit(void)
         WriteData(0x006224EC, -1063 + 39, s32);
     }
 
-    switch (RF_ConfigGetInt(CNF_CART_CARTSPRITE)) {
-
+    switch (RF_ConfigGetInt(CNF_CART_CARTSPRITE))
+    {
         static const double SpriteSubtract = 6.0;
 
-    case CNFE_CART_CARTSPRITE_DREAMCAST:
-        WriteNOP(0x0061CB07, 0x0061CB0C);
-        break;
-
-    case CNFE_CART_CARTSPRITE_ENABLED:
-        ReplaceFloat(0x0061EE25, &SpriteSubtract);
-        break;
+        case CNFE_CART_CARTSPRITE_DREAMCAST:
+        {
+            WriteNOP(0x0061CB07, 0x0061CB0C);
+            break;
+        }
+        case CNFE_CART_CARTSPRITE_ENABLED:
+        {
+            ReplaceFloat(0x0061EE25, &SpriteSubtract);
+            break;
+        }
     }
 
     if (RF_ConfigGetInt(CNF_CART_ZANKI))
@@ -56,4 +74,8 @@ RFM_CartInit(void)
 
         ReplaceFloat(0x0044E78F, &zanki_py);
     }
+
+    // load cart light files
+    WriteNOP( 0x0061A4F9, 0x0061A53D);
+    WriteCall(0x0061A4F9, LoadCartLightFiles);
 }
