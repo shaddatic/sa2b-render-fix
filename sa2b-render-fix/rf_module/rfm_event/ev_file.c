8f85258cb3c9209b526e7a764d07b41bd142e498
Shaddatic <shaddatic@proton.me>
2025-11-06 15:21:02 +0000

    Remove hardcoded movie timing offset

--- sa2b-render-fix/rf_module/rfm_event/ev_file.c
+++ sa2b-render-fix/rf_module/rfm_event/ev_file.c
@@ -273,19 +273,6 @@ EV_LoadEffectFiles(void)
         EvEffectInfo.sound[i] = (EV_EFF_SOUND){ DEF_SOUND_STRUC };
     }
 
-    // add overlay delay
-
-    if ( EventNum != 360 && EventNum != 361 )
-    {
-        for ( int i = 0; i < ARYLEN(EvEffectInfo.overlays); ++i )
-        {
-            if ( EvEffectInfo.overlays[i].type != 0 && EvEffectInfo.overlays[i].frame != 0 )
-            {
-                EvEffectInfo.overlays[i].frame += 15; // + 30 frames
-            }
-        }
-    }
-
     EvEffectInfo.sound[0].WaitVsyncCount = wait_vsync_val;
 }
 
