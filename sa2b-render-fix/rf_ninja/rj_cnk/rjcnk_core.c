31d6c9954e2ad50d8263480e4905b4db493099e6
Shaddatic <shaddatic@proton.me>
2025-11-17 09:40:42 +0000

    Always allow `VND8` drawing. Dreamcast only supported it on MultiDraw variants however there's no examples of them trying to draw VND8 on a non-Multi function, so just enable it by default as a Render Fix extension

--- sa2b-render-fix/rf_ninja/rj_cnk/rjcnk_core.c
+++ sa2b-render-fix/rf_ninja/rj_cnk/rjcnk_core.c
@@ -187,7 +187,8 @@ rjCnkStartPlist(RJS_CNK_STRIP* restrict basest)
             new_flag |= RJD_CXF_NONTEXSPEC;
         }
 
-        _rj_cnk_context_.flag = new_flag;
+        // support VND8 by default, even if Ninja Easy/Simple didn't originally
+        _rj_cnk_context_.flag = new_flag | RJD_CXF_VND8;
     }
 
     _rj_cnk_context_.cull  = -1;
