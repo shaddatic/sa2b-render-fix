ccfd90d198dccd8091f4d95068d4971aaa2959ab
Shaddatic <shaddatic@proton.me>
2026-02-27 09:33:35 +0000

    Add projection module to RF Ninja. Beginning process of reversing the depth buffer to stop Z fightring at larger (and sometimes fairly close) distances

---
+++ sa2b-render-fix/rf_ninja/rj_project.c
@@ -0,0 +1,81 @@
+/********************************/
+/*  Includes                    */
+/********************************/
+/****** Core Toolkit ****************************************************************************/
+#include <samt/core.h>              /* core                                                     */
+#include <samt/writeop.h>           /* WriteJump                                                */
+
+/****** Ninja ***********************************************************************************/
+#include <samt/ninja/ninja.h>       /* ninja                                                    */
+#include <samt/ninja/njcontext.h>   /* ninja context                                            */
+
+/****** Game ************************************************************************************/
+#include <samt/sonic/display.h>     /* resolution                                               */
+
+/****** Render Fix ******************************************************************************/
+#include <rf_core.h>                /* core                                                     */
+
+/****** Self ************************************************************************************/
+#include <rf_ninja/rj_internal.h>  /* parent & siblings                                         */
+
+/********************************/
+/*  Game Variables              */
+/********************************/
+/****** Project *********************************************************************************/
+#define mat_proj_screen             DATA_REF(NJS_MATRIX44, 0x01AF18C0)
+
+/********************************/
+/*  Source                      */
+/********************************/
+/****** Project *********************************************************************************/
+static void
+rjInitProjMatrix(NJS_MATRIX44* dm, f32 view, f32 ratio, f32 near, f32 far)
+{
+    const f32 vtan = tanf(view * 0.5f);
+
+    const f32 clip_range = far - near;
+
+    *dm = (NJS_MATRIX44)
+    {
+        .m[0][0] = 1.f / (vtan * ratio),
+        .m[1][1] = 1.f / (vtan),
+        .m[2][2] = near / clip_range,
+        .m[2][3] = far * (near / clip_range),
+        .m[3][2] = -1.f,
+    };
+
+    mat_proj_screen = (NJS_MATRIX44)
+    {
+        .m[0][0] = 1.f / (vtan * ratio),
+        .m[1][1] = 1.f / (vtan),
+        .m[2][2] = far / clip_range,
+        .m[2][3] = -1.f,
+        .m[3][2] = far * (near / clip_range),
+    };
+}
+
+__declspec(naked)
+static void
+___InitProjMatrix(void)
+{
+    __asm
+    {
+        push        [esp+16]
+        push        [esp+16]
+        push        [esp+16]
+        push        [esp+16]
+        push        eax
+
+        call        rjInitProjMatrix
+
+        add esp,    20
+        retn
+    }
+}
+
+/****** Init ************************************************************************************/
+void
+RJ_ProjectInit(void)
+{
+    WriteJump(0x00427220, ___InitProjMatrix);
+}
