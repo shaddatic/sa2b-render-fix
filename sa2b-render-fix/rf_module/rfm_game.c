22504f170fe5e71d77a713d416f6fef5ba53b195
Shaddatic <shaddatic@proton.me>
2026-09-07 18:15:42 +0100

    Overhaul "no battle" feature, now approaching it from the opposite direction to fix Menu Overhaul, and finally add a config setting.

--- sa2b-render-fix/rf_module/rfm_game.c
+++ sa2b-render-fix/rf_module/rfm_game.c
@@ -6,6 +6,8 @@
 #include <samt/writemem.h>          /* writedata                                                */
 #include <samt/writeop.h>           /* writejump                                                */
 #include <samt/funchook.h>          /* funchook                                                 */
+#include <samt/modinfo.h>           /* mod info                                                 */
+#include <samt/string.h>            /* string match                                             */
 
 /****** GX **************************************************************************************/
 #define SAMT_INCL_FUNCPTRS
@@ -33,10 +35,11 @@
 #include <rf_module/rfm_game/rfg_internal.h> /* children                                        */
 
 /********************************/
-/*  Vars                        */
+/*  Game Defs                   */
 /********************************/
-/****** Static **********************************************************************************/
-static const i32 NoBattle;
+/****** Battle **********************************************************************************/
+#define BattleFlag                  DATA_REF(i32, 0x01A556F4)
+#define BattleFlag2                 DATA_REF(i32, 0x01A556F8)
 
 /********************************/
 /*  Prototypes                  */
@@ -76,15 +79,43 @@ RFM_GlobalInit(void)
 
     if ( CNF_GetInt(CNF_GAME_NOBTL) )
     {
-        WritePointer(0x00458A09 +2, &NoBattle); // game intro,          cmp val
-        WritePointer(0x00667A6E +2, &NoBattle); // screen effect tex,   cmp reg
-        WritePointer(0x00667F0B +2, &NoBattle); // screen effect,       mov
-        WritePointer(0x006682D2 +2, &NoBattle); // screen effect txt,   cmp reg
-        WritePointer(0x0066F8D3 +2, &NoBattle); // menu bg,             cmp val
-        WritePointer(0x0066FEA1 +2, &NoBattle); // alpha title,         cmp val
-        WritePointer(0x00670443 +2, &NoBattle); // alpha title tex,     cmp reg
-        WritePointer(0x0067B717 +2, &NoBattle); // sound test tex,      cmp val
-        WritePointer(0x00688B04 +2, &NoBattle); // 2p stage sel,        cmp val
+        BattleFlag = FALSE;
+
+        // set things we want to keep enabled to the second battle flag
+        WritePointer(0x0044476E +2, &BattleFlag2); // all emblems ach,      cmp val
+        WritePointer(0x0044504E +2, &BattleFlag2); // other achieves,       cmp val
+        WritePointer(0x00445217 +2, &BattleFlag2); // menu theme set,       cmp val
+        WritePointer(0x004568C2 +2, &BattleFlag2); // something steam,      cmp reg
+//      WritePointer(0x00458A09 +2, &BattleFlag2); // game intro,           cmp val
+        WritePointer(0x00542963 +2, &BattleFlag2); // karate something,     cmp reg
+        WritePointer(0x00581891 +2, &BattleFlag2); // chao cave menu,       mov reg
+        WritePointer(0x0058A047 +2, &BattleFlag2); // bmarket themes,       cmp val
+        WritePointer(0x0058B8F8 +2, &BattleFlag2); // bmarket themes 2,     cmp val
+        WritePointer(0x005A882E +2, &BattleFlag2); // chao cave arrows,     cmp reg
+        WritePointer(0x005A894D +2, &BattleFlag2); // chao cave arrows,     cmp reg
+//      WritePointer(0x00667A6E +2, &BattleFlag2); // screen effect tex,    cmp reg
+//      WritePointer(0x00667F0B +2, &BattleFlag2); // screen effect,        mov reg
+//      WritePointer(0x006682D2 +2, &BattleFlag2); // screen effect txt,    cmp reg
+        WritePointer(0x0066A039 +1, &BattleFlag2); // 2p costumes,          mov eax
+        WritePointer(0x0066A69D +2, &BattleFlag2); // btl player select,    cmp reg
+        WritePointer(0x0066B191 +2, &BattleFlag2); // btl stg select,       mov reg
+        WritePointer(0x0066B344 +2, &BattleFlag2); // btl something,        cmp val
+        WritePointer(0x0066B420 +2, &BattleFlag2); // btl something,        cmp val
+        WritePointer(0x0066B98A +2, &BattleFlag2); // btl next stage?,      cmp val
+        WritePointer(0x0066B9C6 +2, &BattleFlag2); // btl next stage?,      cmp val
+        WritePointer(0x0066BA78 +2, &BattleFlag2); // btl next stage?,      cmp val
+        WritePointer(0x0066BA87 +2, &BattleFlag2); // btl next stage?,      cmp val
+        WritePointer(0x0066C960 +2, &BattleFlag2); // menu background?,     cmp reg
+        WritePointer(0x0066CA80 +2, &BattleFlag2); // menu background?,     cmp reg
+        WritePointer(0x0066DA03 +2, &BattleFlag2); // settings something?,  cmp reg
+        WritePointer(0x0066DF02 +2, &BattleFlag2); // settings something?,  cmp reg
+//      WritePointer(0x0066F8D3 +2, &BattleFlag2); // menu bg,              cmp val
+//      WritePointer(0x0066FEA1 +2, &BattleFlag2); // alpha title,          cmp val
+//      WritePointer(0x00670443 +2, &BattleFlag2); // alpha title tex,      cmp reg
+//      WritePointer(0x0067B717 +2, &BattleFlag2); // sound test tex,       cmp val
+        WritePointer(0x00687AE8 +2, &BattleFlag2); // btl something?,       cmp val
+        WritePointer(0x00687E1F +2, &BattleFlag2); // btl select something?,cmp val
+//      WritePointer(0x00688B04 +2, &BattleFlag2); // 2p stage sel,         cmp val
     }
 
     if ( CNF_GetInt(CNF_GAME_IGBIX) )
