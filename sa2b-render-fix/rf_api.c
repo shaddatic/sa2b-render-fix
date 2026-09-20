5f072a2f442a9567a7bc41bc8cc7a3fdaa922b8b
Shaddatic <shaddatic@proton.me>
2026-09-20 10:00:36 +0100

    Bump version

--- sa2b-render-fix/rf_api.c
+++ sa2b-render-fix/rf_api.c
@@ -34,7 +34,7 @@ const RFAPI_CORE rfapi_core =
 {
     .version = RFAPI_CORE_VER,
 
-    .modver = { 1,5,5,0 },
+    .modver = { 1,5,5,1 },
 
     .api_ctrl       = &rfapi_control,
     .api_config     = &rfapi_config,
